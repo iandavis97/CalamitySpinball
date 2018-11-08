3eff3003f8b4a7987d0c862f986ec24159a73cf5
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2018-11-08 00:49:36 -0500

    Added plunger, bumper, and paddle

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+	[SerializeField]
+	GameObject forceApplicator;	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			GetComponent<Rigidbody>().AddForceAtPosition(Vector3.up * 15, forceApplicator.transform.position, ForceMode.Impulse);
+		}
+	}
+}
