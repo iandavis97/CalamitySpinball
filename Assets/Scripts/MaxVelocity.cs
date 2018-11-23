f47240a7e3add26edce9b21cde1541b688a9750b
mxo9722 <mxo9722@rit.edu>
2018-11-23 17:29:11 -0500

    Added slingshots
    Made a few more changes to the board including the addition of the slingshots (or side bumpers as they've been called earlier).

---
+++ Assets/Scripts/MaxVelocity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaxVelocity : MonoBehaviour {
+
+	public float MaxVel=10;
+	Rigidbody rigidB;
+
+	// Use this for initialization
+	void Start () {
+		rigidB = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rigidB.velocity.magnitude > MaxVel) 
+		{
+			rigidB.velocity = rigidB.velocity.normalized*MaxVel;
+		}
+	}
+}
---
+++ Assets/Scripts/SlingShot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShot : MonoBehaviour {
+	[SerializeField]
+	float force;
+	[SerializeField]
+	GameObject direction;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		Vector3 forceDir = Vector3.Normalize(direction.transform.position - this.transform.position) * force;
+		other.gameObject.GetComponent<Rigidbody>().AddForce(forceDir, ForceMode.Impulse);
+	}
+}
