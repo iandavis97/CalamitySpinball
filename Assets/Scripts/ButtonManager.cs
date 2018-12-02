b29a1599fbf348763353084824c4238a9b3f12c5
Mark DelFavero <markadelf@gmail.com>
2018-12-02 13:26:15 -0500

    Worked on menus

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject controls;
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void TitleScreenButton()
+    {
+        SceneManager.LoadScene("Title_Screen");
+    }
+
+    public void EndGameScreen()
+    {
+        SceneManager.LoadScene("End_Game_Screen");
+    }
+
+    public void showControls()
+    {
+        if(controls != null)
+        {
+            if (controls.activeInHierarchy)
+            {
+                controls.SetActive(false);
+            }
+            else
+            {
+                controls.SetActive(true);
+            }
+        }
+    }
+
+}
