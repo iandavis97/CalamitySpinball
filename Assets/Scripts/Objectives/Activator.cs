7721109061e922b6e83d0557eaaed226535e4c2f
Mark DelFavero <markadelf@gmail.com>
2018-12-06 12:49:37 -0500

    Activator Groups

---
+++ Assets/Scripts/Objectives/Activator.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour {
+
+    public GameObject Active;
+
+    public GameObject InActive;
+
+    public bool Reversible;
+
+    public bool State { get; private set; }
+
+    private ActivatorGroup group;
+
+    private Collider col;
+
+    // Use this for initialization
+    void Awake () {
+        Active.SetActive(State);
+        InActive.SetActive(!State);
+        col = GetComponent<Collider>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetGroup(ActivatorGroup group)
+    {
+        this.group = group;
+    }
+
+    public void HardReset()
+    {
+        State = false;
+        Active.SetActive(State);
+        InActive.SetActive(!State);
+        Debug.Log("Reset");
+        col.enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (group.State == Objective.ObjectiveState.Enabled && other.gameObject.tag == "Ball")
+        {
+            Hit();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (group.State == Objective.ObjectiveState.Enabled && collision.gameObject.tag == "Ball")
+        {
+            Hit();
+        }
+    }
+
+    private void Hit()
+    {
+        State = Reversible ? !State : true;
+        Active.SetActive(State);
+        InActive.SetActive(!State);
+        group.Alert();
+        col.enabled = State ? Reversible : true;
+    }
+}
---
+++ Assets/Scripts/Objectives/Objective.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Objective : MonoBehaviour {
+
+    public enum ObjectiveState
+    {
+        Locked,
+        Enabled,
+        Finished
+    }
+
+    // Links 
+    // Don't change these at runtime
+    public List<Objective> PreReqs;
+    private List<Objective> dependants;
+
+    public ObjectiveState State { get; private set; }
+
+    private void Awake()
+    {
+        dependants = new List<Objective>();
+    }
+
+    // Use this for initialization
+    protected virtual void Start ()
+    {
+        State = PreReqs.Count > 0 ? ObjectiveState.Locked : ObjectiveState.Enabled;
+        for (int i = 0; i < PreReqs.Count; i++)
+        {
+            PreReqs[i].dependants.Add(this);
+        }
+        OnStateChange();
+    }
+
+    // Declares the objective as complete to its dependants
+    protected void Finish()
+    {
+        if (dependants.Count > 0)
+        {
+            State = ObjectiveState.Finished;
+            for (int i = 0; i < dependants.Count; i++)
+            {
+                dependants[i].CheckPreReqs();
+            }
+        }
+        else
+        {
+            ResetObjectiveHierarchy();
+        }
+        OnStateChange();
+    }
+
+    // Check if prereqs are met
+    private void CheckPreReqs()
+    {
+        State = ObjectiveState.Enabled;
+        for (int i = 0; i < PreReqs.Count; i++)
+        {
+            if(PreReqs[i].State != ObjectiveState.Finished)
+            {
+                State = ObjectiveState.Locked;
+            }
+        }
+        OnStateChange();
+    }
+
+    // Called by end points to reset the objectives
+    private void ResetObjectiveHierarchy()
+    {
+        if(PreReqs.Count < 0)
+        {
+            for (int i = 0; i < PreReqs.Count; i++)
+            {
+                PreReqs[i].ResetObjectiveHierarchy();
+            }
+            State = ObjectiveState.Locked;
+        }
+        else
+        {
+            State = ObjectiveState.Enabled;
+        }
+    }
+
+    protected abstract void OnStateChange();
+}
