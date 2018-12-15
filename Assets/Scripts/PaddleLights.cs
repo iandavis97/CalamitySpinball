f61a804b61f05b428d485037d2da5a725f8e9c0b
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2018-12-15 15:06:28 -0500

    Added FX to SpaceBoard

---
+++ Assets/Scripts/PaddleLights.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleLights : MonoBehaviour 
+{
+	[SerializeField]
+	Light[] leftPaddleLights;
+	[SerializeField]
+	Light[] rightPaddleLights;
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			StartCoroutine(ToggleLeftLights(0, 5, .5f));
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			StartCoroutine(ToggleRightLights(0, 5, .5f));
+		}
+	}
+	IEnumerator ToggleLeftLights(int times, int limit, float delay)
+	{
+		for (int i = 0; i < leftPaddleLights.Length; ++i)
+		{
+			leftPaddleLights[i].gameObject.SetActive(!leftPaddleLights[i].gameObject.activeInHierarchy);
+		}
+		yield return new WaitForSeconds(delay);
+		if (times < limit)
+		{
+			++times;
+			StartCoroutine(ToggleLeftLights(times, limit, delay));
+		}
+	}
+
+	IEnumerator ToggleRightLights(int times, int limit, float delay)
+	{
+		for (int i = 0; i < rightPaddleLights.Length; ++i)
+		{
+			rightPaddleLights[i].gameObject.SetActive(!rightPaddleLights[i].gameObject.activeInHierarchy);
+		}
+		yield return new WaitForSeconds(delay);
+		if (times < limit)
+		{
+			++times;
+			StartCoroutine(ToggleRightLights(times, limit, delay));
+		}
+	}
+}
---
+++ Assets/Scripts/PlungerLights.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlungerLights : MonoBehaviour 
+{
+	[SerializeField]
+	Light[] lights;	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyUp(KeyCode.DownArrow))
+		{
+			StartCoroutine(ToggleLights(0, 5, .5f));
+		}
+	}
+
+	IEnumerator ToggleLights(int times, int limit, float delay)
+	{
+		for (int i = 0; i < lights.Length; ++i)
+		{
+			lights[i].gameObject.SetActive(!lights[i].gameObject.activeInHierarchy);
+		}
+		yield return new WaitForSeconds(delay);
+		if (times < limit)
+		{
+			++times;
+			StartCoroutine(ToggleLights(times, limit, delay));
+		}
+
+	}
+}
